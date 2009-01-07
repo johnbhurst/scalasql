9842977cb818a07bdec51b705cb1c72fadba6e92
John Hurst <jbhurst@attglobal.net>
2009-01-07 16:39:29 +1300

    Added JUnit test class; needs to be filled in.

--- bin/runtest.sh
+++ bin/runtest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 scala -cp out/production/scalasql/:out/test/scalasql/:lib/ojdbc14-10.2.0.3.0.jar \
-  nz.co.skepticalhumorist.sql.Test
+  nz.co.skepticalhumorist.sql.TestApp
 
---
+++ src/test/nz/co/skepticalhumorist/sql/DbTest.scala
@@ -0,0 +1,22 @@
+// $Id$
+// John Hurst (jbhurst@attglobal.net)
+// 2009-01-07
+
+package nz.co.skepticalhumorist.sql
+
+import org.junit.Before
+import org.junit.Test
+
+class DbTest {
+
+  @Before
+  def setupData {
+    println("setupData():")
+  }
+
+  @Test
+  def testExecute {
+    println("testExecute():")
+  }
+
+}
